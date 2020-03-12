2646b8b670ab6c052920b174c9b3463d65d850c2
noahamac <56001784+noahamac@users.noreply.github.com>
2020-03-12 16:58:47 -0700

    Update viz-inspector.js

--- viz-inspector.js
+++ viz-inspector.js
@@ -1,11 +1,7 @@
 var tree;
 function copyToClipboard(text) {
     var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
     document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
     dummy.value = text;
     dummy.select();
     document.execCommand("copy");
